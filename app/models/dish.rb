793856fd305b0689f490e248e64118b7ac4f7dbb
emjose <emjose@gmail.com>
2020-09-25 09:44:18 -0400

    testing

---
+++ app/models/dish.rb
@@ -0,0 +1,6 @@
+class Dish < ApplicationRecord
+    has_many :likes 
+    has_many :comments
+    has_many :users, through: :likes 
+    has_many :users, through: :comments
+end
---
+++ db/migrate/20200925133118_create_dishes.rb
@@ -0,0 +1,12 @@
+class CreateDishes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :dishes do |t|
+      t.string :origin
+      t.string :name
+      t.string :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
